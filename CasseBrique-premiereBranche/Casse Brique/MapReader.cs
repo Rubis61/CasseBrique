3f535acf36878f4108300e49215f71340cdeffdc
Anni <pierre_henri_debris@orange.fr>
2014-11-19 10:52:14 +0100

    ajout MapReader

---
+++ CasseBrique-premiereBranche/Casse Brique/MapReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Casse_Brique
+{
+    class MapReader
+    {
+        
+        private static string line;
+        public static string[] tabLine = new string[1];
+        private static string[][] split = new string[15][];
+        private static string repository = Directory.GetCurrentDirectory();
+        private static int y = 0;
+
+    private static void readLigneFichier()
+    {
+
+    System.IO.StreamReader file = new System.IO.StreamReader(@repository+"\\level\\level1.txt");
+
+    while((line = file.ReadLine()) != null)
+    {
+        tabLine[y] = line;
+        y++;
+
+    }
+    file.Close();
+    }
+
+    private static void SplitLigne()
+    {
+        for (int i = 0; i < tabLine.Length; i++)
+        {
+            split[i] = tabLine[i].Split(' ');
+        }
+
+    }
+        
+    public static string[][] getMap()
+    {
+        return split;
+    }
+
+    }
+}
